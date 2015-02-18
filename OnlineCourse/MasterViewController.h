304ad66593cfdfbec7afc6c39a14890c6eca1ccd
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-02-18 14:51:59 -0500

    Initial Commit: Creating the project

---
+++ OnlineCourse/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  OnlineCourse
+//
+//  Created by Masih Tabrizi on 2/18/15.
+//  Copyright (c) 2015 Masih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
