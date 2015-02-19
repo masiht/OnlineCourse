c71c6128f047e6c06905d486e34a823c44c807eb
Merritt Tidwell <merritt.tidwell@gmail.com>
2015-02-19 13:29:54 -0500

    changes to storyboard and added loginViewController

---
+++ LogInViewController.h
@@ -0,0 +1,14 @@
+//
+//  LogInViewController.h
+//  OnlineCourse
+//
+//  Created by Merritt Tidwell on 2/19/15.
+//  Copyright (c) 2015 Masih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *viewFields;
+
+@end
---
+++ LogInViewController.m
@@ -0,0 +1,45 @@
+//
+//  LogInViewController.m
+//  OnlineCourse
+//
+//  Created by Merritt Tidwell on 2/19/15.
+//  Copyright (c) 2015 Masih. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    self.viewFields.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.viewFields.layer.borderWidth = 1;
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+// Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
